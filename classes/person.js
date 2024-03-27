67326c0be8d0fc80564e9196b7b813996ab3226f
dcsiona@gmail.com <dcsiona@gmail.com>
2024-03-27 16:02:08 -0400

    another commit

--- classes/person.js
+++ classes/person.js
@@ -1,25 +1,24 @@
 const Dog = require('./dog');
 
-class Person {
-  constructor(name, age, dogs = []) {
+function Person (name, age, dogs = []) {
     this.name = name;
     this.age = age;
     this.dogs = dogs;
-  }
+}
 
-  addDog(dog) {
+Person.prototype.addDog = function(dog) {
     if (!(dog instanceof Dog)) {
       throw new TypeError("Can only add dogs!");
     }
     this.dogs.push(dog);
-  }
+  };
 
-  walkDogs() {
+ Person.prototype.walkDogs = function() {
     this.dogs.forEach((dog) => {
       console.log(`walking ${dog.name}`);
     });
-  }
-}
+  };
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
