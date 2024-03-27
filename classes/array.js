feb6640240a8df16a925b33214cb074cf95aaa26
dcsiona@gmail.com <dcsiona@gmail.com>
2024-03-27 16:16:53 -0400

    another commit

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,16 @@
 // Your code here
+
+// Monkey-patching array class
+Array.prototype.isEqual = function(array) {
+    // Check if lengths are equal
+    if (this.length !== array.length) {
+        return false;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] !== array[i]) {
+            return false;
+        }
+    }
+    return true;
+}
